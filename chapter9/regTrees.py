ce598cc03a25bd564df1034131db1e6c2f01ecc7
Com-Mean <out_1018@outlook.com>
2014-09-12 01:30:42 +0800

    add forecast process

--- chapter9/regTrees.py
+++ chapter9/regTrees.py
@@ -160,3 +160,35 @@ def prune(tree, testData):
             return tree
     else:
         return tree
+
+# forcast process
+def regTreeEval(model, inDat):
+    return float(model)
+
+def modelTreeEval(model, inDat):
+    n = shape(inDat)[1]
+    X = mat(ones((1, n+1)))
+    X[:, 1:n+1] = inDat
+    return float(X * model)
+
+def treeForecast(tree, inData, modelEval = regTreeEval):
+    if not isTree(tree):
+        return modelEval(tree, inData)
+    if inData[tree['spInd']] > tree['spVal']:
+        if isTree(tree['left']):
+            return treeForcast(tree['left'], inData, modelEval)
+        else:
+            return modelEval(tree['left'], inData)
+    else:
+        if isTree(tree['right']):
+            return treeForcast(tree['right'], inData, modelEval)
+        else:
+            return modelEval(tree['right'], inData)
+
+def createForecast(tree, testData, modelEval=regTreeEval):
+    m = len(testData)
+    yHat = mat(zeros((m,1)))
+    for i in range(m):
+        yHat[i, 0] = treeForecast(tree, testData, modelEval)
+    return yHat
+
